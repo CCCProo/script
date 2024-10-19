ef411ab230b5009aa31eabe7dd8980e2bb353984
Kirill Shishkov <93086215+CCCProo@users.noreply.github.com>
2024-10-19 20:29:46 +0300

    Update tool.lua

--- tt/tool.lua
+++ tt/tool.lua
@@ -1,23 +1,31 @@
 local animation_id = 83185031265740
 local player = game.Players.LocalPlayer
-local character = player.Character or player.CharacterAdded:Wait()
-local humanoid = character:WaitForChild("Humanoid")
-local animation = Instance.new("Animation")
-animation.AnimationId = "rbxassetid://" .. tostring(animation_id)
 
 local isPlaying = false
 
-script.Parent.MouseButton1Up:Connect(function()
-	print(isPlaying)
-	if not isPlaying then
-		isPlaying = true
+for _, button in ipairs(script.Parent.Buttons:GetChildren()) do
+	
+	print(button)
+	
+	button.MouseButton1Up:Connect(function()
+		
+		local character = player.Character or player.CharacterAdded:Wait()
+		local humanoid = character:WaitForChild("Humanoid")
+		local animation = Instance.new("Animation")
+		animation.AnimationId = "rbxassetid://" .. tostring(button.IdAnimation.Value)
+				
+		if not isPlaying then
+			isPlaying = true
 
-		local animator = humanoid:FindFirstChildOfClass("Animator")
-		local animationTrack = animator:LoadAnimation(animation)
-		animationTrack:Play()
+			local animator = humanoid:FindFirstChildOfClass("Animator")
+			local animationTrack = animator:LoadAnimation(animation)
+			animationTrack:Play()
 
-		animationTrack.Stopped:Connect(function()
-			isPlaying = false
-		end)
-	end
-end)
+			animationTrack.Stopped:Connect(function()
+				isPlaying = false
+			end)
+			
+		end
+		
+	end)
+end
